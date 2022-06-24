b27fece502e6e16e4c26a7cc56131d1c20d8c4e0
doncity-tech <princewill.opara@outlook.com>
2022-06-24 08:30:15 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+//Write a javascript code that prints out your name, height, and country on the screen\
+let fullName = "Opara Princewill";
+let height = 5.7;
+let country = "Nigeria";
+console.log(fullName + " " + height + "m " + country)
